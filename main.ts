da4217299417faec507604aab8c96c144dc8a9d8
CmdHebiSensei（コマへび先生） <cmd.hebi.sensei@gmail.com>
2025-10-08 05:31:11 +0900

    追加

--- main.ts
+++ main.ts
@@ -107,7 +107,9 @@ namespace EduTutorialTest {
     //% particle.defl=MainParticle.HeartParticle
     export function emitParticleAtProjectiles(projectile: ProjectileEntity, particle: MainParticle) {
         // それぞれのエンティティの足元でパーティクルを生成
-        player.say(`say execute at @e[type=${getEntityId(projectile)}] run particle ${getParticleId(particle)} ~ ~ ~`)
+        player.say(`aaaaa`)
+        player.say(`execute at @e[type=${getEntityId(projectile)}] run particle ${getParticleId(particle)} ~ ~ ~`)
+        player.runChatCommand(`execute at @e[type=${getEntityId(projectile)}] run particle ${getParticleId(particle)} ~ ~ ~`)
         // player.runChatCommand(`/say execute at @e[type=${getEntityId(projectile)}] run particle ${getParticleId(particle)} ~ ~ ~`)
     }
 
