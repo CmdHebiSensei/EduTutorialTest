8410c60d70cddf00623d256b23d60f4d669523ae
CmdHebiSensei（コマへび先生） <cmd.hebi.sensei@gmail.com>
2025-10-08 07:24:07 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -91,7 +91,8 @@ function getParticleId(particle: MainParticle): string {
 //% color=#5B9BD5 icon="\uf06e" block="EduTutorialTest"
 namespace EduTutorialTest {
 
-    //% blockId=hebi_custom_block block="♻%seconds びょう くりかえす"
+    //% block="♻%seconds びょう くりかえす"
+    //% jres=Item.Clock
     //% handlerStatement=1
     //% seconds.defl=3
     export function repeatForSeconds(seconds: number, handler: () => void) {
