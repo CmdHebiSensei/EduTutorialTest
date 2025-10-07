431ce0c211d292c3a5aebdeb6af2e881d2606519
CmdHebiSensei（コマへび先生） <cmd.hebi.sensei@gmail.com>
2025-10-08 04:43:24 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ declare const enum ProjectileItem {
     //% block="卵"
     Egg,
     //% block="ポーション"
-    Potion
+    Potion,
 }
 
 // itemId => enum
@@ -31,37 +31,62 @@ function getItemId(item: ProjectileItem): number {
 }
 
 // 飛翔物
-enum ProjectileEntity {
+declare const enum ProjectileEntity {
     //% block="矢"
-    Arrow = "minecraft:arrow",
+    Arrow,
     //% block="雪玉"
-    Snowball = "minecraft:snowball",
+    Snowball,
     //% block="トライデント"
-    Trident = "minecraft:thrown_trident",
+    Trident,
     //% block="ウィンドチャージ"
-    WindCharge = "minecraft:wind_charge",
+    WindCharge,
     //% block="卵"
-    Egg = "minecraft:egg",
+    Egg,
     //% block="ポーション",
-    Potion = "minecraft:potion"
+    Potion,
+}
+
+// entity => id
+function getEntityId(entity: ProjectileEntity): string {
+    switch (entity) {
+        case ProjectileEntity.Arrow: return "minecraft:arrow";
+        case ProjectileEntity.Snowball: return "minecraft:snowball";
+        case ProjectileEntity.Trident: return "minecraft:thrown_trident";
+        case ProjectileEntity.WindCharge: return "minecraft:wind_charge_projectile";
+        case ProjectileEntity.Egg: return "minecraft:egg";
+        case ProjectileEntity.Potion: return "minecraft:splash_potion";
+        default: throw `Unrecognized ProjectileEntity enum value: ${entity}`
+    }
 }
 
 // パーティクル
 declare const enum MainParticle {
     //% block="こうかつきの矢の もやもや"
-    ArrowSpellEmitter = "minecraft:arrow_spell_emitter",
+    ArrowSpellEmitter,
     //% block="ふうせんの キラキラ"
-    BalloonGasParticle = "minecraft:balloon_gas_particle",
+    BalloonGasParticle,
     //% block="ひのこ"
-    BasicFlameParticle = "minecraft:basic_flame_particle",
+    BasicFlameParticle,
     //% block="あおい ひのこ"
-    BlueFlameParticle = "minecraft:blue_flame_particle",
+    BlueFlameParticle,
     //% block="さくもつが そだつときの キラキラ"
-    CropGrowthEmitter = "minecraft:crop_growth_emitter",
+    CropGrowthEmitter,
     //% block="ハート"
-    HeartParticle = "minecraft:heart_particle"
+    HeartParticle,
 }
 
+// particle => id
+function getParticleId(particle: MainParticle): string {
+    switch (particle) {
+        case MainParticle.ArrowSpellEmitter: return "minecraft:arrow_spell_emitter";
+        case MainParticle.BalloonGasParticle: return "minecraft:balloon_gas_particle";
+        case MainParticle.BasicFlameParticle: return "minecraft:basic_flame_particle";
+        case MainParticle.BlueFlameParticle: return "minecraft:blue_flame_particle";
+        case MainParticle.CropGrowthEmitter: return "minecraft:crop_growth_emitter";
+        case MainParticle.HeartParticle: return "minecraft:heart_particle";
+        default: throw `Unrecognized MainParticle enum value: ${particle}`
+    }
+}
 //% color=#5B9BD5 icon="\uf06e" block="EduTutorialTest"
 namespace EduTutorialTest {
 
@@ -80,16 +105,15 @@ namespace EduTutorialTest {
     //% block="%projectile のところに %particle の パーティクルをだす"
     //% projectile.defl=ProjectileEntity.Arrow
     //% particle.defl=MainParticle.HeartParticle
-    export function emitParticleAtProjectiles(particle: MainParticle, projectile: ProjectileEntity) {
+    export function emitParticleAtProjectiles(projectile: ProjectileEntity, particle: MainParticle) {
         // それぞれのエンティティの足元でパーティクルを生成
-        player.runChatCommand(`execute at @e[type=${projectile}] run particle ${particle} ~ ~ ~`)
+        player.runChatCommand(`execute at @e[type=${getEntityId(projectile)}] run particle ${getParticleId(particle)} ~ ~ ~`)
     }
 
     //% block="%item をつかって うちだしたとき"
     //% item.defl=ProjectileItem.Snowball
     export function onProjectileItemUsed(item: ProjectileItem, handler: () => void) {
-        const itemEnum = getItemId(item)
         // MakeCodeの標準イベント：プレイヤーが指定アイテムを使用したとき
-        if (itemEnum >= 0) player.onItemInteracted(itemEnum, handler)
+        player.onItemInteracted(getItemId(item), handler)
     }
 }
